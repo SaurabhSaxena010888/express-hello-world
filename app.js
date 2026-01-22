7c0522d29d203f90a71aa8743d34469c1b22ffe8
SaurabhSaxena010888 <65444514+SaurabhSaxena010888@users.noreply.github.com>
2026-01-23 02:49:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,56 @@
 const express = require("express");
+const admin = require("firebase-admin");
+
+/* -------------------- FIREBASE INIT -------------------- */
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+/* -------------------- APP SETUP -------------------- */
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+/* -------------------- ROUTES -------------------- */
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Home route (Render default page)
+app.get("/", (req, res) => res.type("html").send(html));
+
+// 🔥 Firebase connectivity test
+app.get("/firebase-test", async (req, res) => {
+  try {
+    const snapshot = await db
+      .collection("questionnaires")
+      .limit(1)
+      .get();
+
+    res.json({
+      success: true,
+      docsFound: snapshot.size,
+    });
+  } catch (err) {
+    console.error("Firebase test error:", err);
+    res.status(500).json({
+      success: false,
+      error: err.message,
+    });
+  }
+});
+
+/* -------------------- SERVER -------------------- */
+const server = app.listen(port, () =>
+  console.log(`Server running on port ${port}`)
+);
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
+/* -------------------- HTML -------------------- */
 const html = `
 <!DOCTYPE html>
 <html>
@@ -29,7 +71,7 @@ const html = `
       @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
       @font-face {
         font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2");
         font-style: normal;
         font-weight: 700;
       }
@@ -47,7 +89,6 @@ const html = `
         position: absolute;
         top: 50%;
         left: 50%;
-        margin-right: -50%;
         transform: translate(-50%, -50%);
       }
     </style>
@@ -58,4 +99,5 @@ const html = `
     </section>
   </body>
 </html>
-`
+`;
+
